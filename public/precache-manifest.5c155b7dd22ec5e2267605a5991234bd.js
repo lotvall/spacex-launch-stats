845df601e92835224f2c6d7101a70daa8a9130c0
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-01-26 11:01:28 +0100

    finished react

---
+++ public/precache-manifest.5c155b7dd22ec5e2267605a5991234bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5fc13dee52ddaae62b25",
+    "url": "/static/js/main.5fc13dee.chunk.js"
+  },
+  {
+    "revision": "e86526263e116bf9cad4",
+    "url": "/static/js/1.e8652626.chunk.js"
+  },
+  {
+    "revision": "5fc13dee52ddaae62b25",
+    "url": "/static/css/main.f5505851.chunk.css"
+  },
+  {
+    "revision": "e86526263e116bf9cad4",
+    "url": "/static/css/1.04c9d0ad.chunk.css"
+  },
+  {
+    "revision": "dd416e7d1b20c8120fed6f91b59c083c",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const graphqlHttp = require('express-graphql')
 const schema = require('./schema')
+const cors = require('cors')
+const path = require('path')
 
 
 const app = express();
+
+app.use(cors())
 
 app.use('/graphql', graphqlHttp({
   schema,
   graphiql: true
 }));
+
+app.use(express.static('public'))
+
+app.get('*',(req, res) => {
+  res.sendFile(path.resovle(__dirname, 'public', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000
 
